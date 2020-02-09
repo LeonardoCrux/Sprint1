abb4da9e36312d17356aff40f7b599717458ecaa
aruanaleixo <49173061+aruanaleixo@users.noreply.github.com>
2020-02-09 12:26:57 -0300

    Página de Login + mudança para página de Cadastro

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cadastro = (Button) findViewById(R.id.button);
+        cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirCadastro();
+            }
+        });
+
+
     }
+
+    public void abrirCadastro(){
+        Intent intent = new Intent(this, Button.class);
+        startActivity(intent);
+
+    }
+
 }
